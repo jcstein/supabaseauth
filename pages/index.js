9f80743c80ecdc68aa65a405d96164b999d77f4b
jcstein <mail@jcstein.dev>
2021-11-10 14:04:12 -0500

    code sign in

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,8 @@ export default function Home() {
         👋 Welcome to <a href="https://twitter.com/JoshCStein">jcs.sol!</a>
       </h1>
       <p className={styles.description}>
-       🔐 sign in above to see what I am building today
+       <code className={styles.code}>🔐 Sign In</code>
+       {' '}above to see what I am building today
       </p>
     </main>
   </div> 
