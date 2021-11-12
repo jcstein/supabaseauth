43e661d05f3380e1b3e2cb43afb94e37aae694be
jcstein <mail@jcstein.dev>
2021-11-11 23:13:01 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
-    publicExcludes: [
-      '!robots.txt',
-      'sitemap.xml.gz',
-    ],
     register: true,
     skipWaiting: true,
   },
